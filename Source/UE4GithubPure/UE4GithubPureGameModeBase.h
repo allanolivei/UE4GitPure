a6f0ec610a016eb9fb399834ebc2398ef9d4f7ee
Allan Oliveira <allanolivei@gmail.com>
2020-03-23 10:27:28 -0300

    init

---
+++ Source/UE4GithubPure/UE4GithubPureGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4GithubPureGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4GITHUBPURE_API AUE4GithubPureGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
